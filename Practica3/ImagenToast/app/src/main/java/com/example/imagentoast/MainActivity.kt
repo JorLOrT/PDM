33c0163a6a28c53c78bf6c44465cb9c0e38142b0
JorLOrT <jortizc@ulasalle.edu.pe>
2024-04-13 20:00:28 -0500

    Creacion Ejercicio 1

---
+++ Practica3/ImagenToast/app/src/main/java/com/example/imagentoast/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.imagentoast
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val imgC:ImageView = findViewById(R.id.mapache)
+        imgC.setOnClickListener {
+            Toast.makeText(applicationContext, "Pedro Mapache", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
