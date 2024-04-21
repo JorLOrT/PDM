2c25b84d1a01c27c8ffd875b88f04502be7c0e28
JorLOrT <jortizc@ulasalle.edu.pe>
2024-04-20 20:25:10 -0500

    Terminado ejercicio1

--- Practica4/Ejercicio1/app/src/main/java/com/example/myapplication/VentanaDos.kt
+++ Practica4/Ejercicio1/app/src/main/java/com/example/myapplication/VentanaDos.kt
@@ -5,8 +5,6 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class VentanaDos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
